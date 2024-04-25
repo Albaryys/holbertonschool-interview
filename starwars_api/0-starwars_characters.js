5ee016a4de5a29e1efb64679fc8b03f4f80b195c
Albaryys <a.tily.pro@gmail.com>
2024-04-25 14:30:24 +0200

    Refactor Star Wars character retrieval code

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
-const request = require('request');
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
-  if (!err) {
-    const characters = JSON.parse(body).characters;
-    recursionRequestPrint(characters, 0);
-  }
+const movieId = process.argv[2];
+
+request(`https://swapi-api.hbtn.io/api/films/${movieId}`, (err, res, body) => {
+    if (!err) {
+        const characters = JSON.parse(body).characters;
+        recursionRequestPrint(characters, 0);
+    }
 });
 
-function recursionRequestPrint (url, index) {
-  request(url[index], (err, res, body) => {
-    if (!err) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < url.length) recursionRequestPrint(url, ++index);
-    }
-  });
+function recursionRequestPrint(url, index) {
+    request(url[index], (err, res, body) => {
+        if (!err) {
+            console.log(JSON.parse(body).name);
+            if (index + 1 < url.length) recursionRequestPrint(url, ++index);
+        }
+    });
 }
